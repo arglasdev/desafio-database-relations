7a8ad9110246f8d3468cf7526784ed7611af1d4f
artur <arglasdev@gmail.com>
2020-08-12 22:17:08 -0300

    cadastro/validação customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,22 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) { }
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const alreadyRegisterCustomer = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (alreadyRegisterCustomer) {
+      throw new AppError('Email already registered.');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+    return customer;
   }
 }
 
